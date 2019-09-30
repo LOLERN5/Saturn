19c6ff12e2c2e8f64bf3933d19b0b1275a7e243b
LOLERN5 <52764549+LOLERN5@users.noreply.github.com>
2019-09-30 14:40:30 -0700

    + Code
    Added the main code.

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,1 +1,33 @@
+-- Saturn ok
+local Saturn = {}
+local LP = game.Players.LocalPlayer
+ 
+-- Stuff
+Saturn.Whitelists = {}
+Saturn.Settings = {
+UseWhitelist = true,
+WhitelistMethod = "playername",
+Url = "https://pastebin.com/raw/blahblahblah"
+}
 
+Saturn.Whitelists.pname = {"player1","player2","player3"} -- Yeah Just add a ,"plrname" then you'll be fine.
+
+function StartSaturn()
+loadstring(game:HttpGet(Saturn.Settings.Url))()
+end
+
+function StartCheck(wlmethod,plr)
+if wlmethod == "playername" then
+for _,v in pairs(Saturn.Whitelists.pname) do
+if plr == v then
+StartSaturn()
+     end
+   end
+ end
+end
+
+if Saturn.Settings.UseWhitelist == true then
+ if Saturn.Settings.WhitelistMethod == "playername" then
+ StartCheck("playername",LP.Name)
+ end
+end
